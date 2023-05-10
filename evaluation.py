73b2bd454c7de0cd54ec7d96b5891d78c7823ed3
Jacob Hansen <85134229+jacob-hansen@users.noreply.github.com>
2023-05-10 00:41:44 -0400

    (just the code, not runnable script)

---
+++ evaluation.py
@@ -0,0 +1,21 @@
+import os 
+import sys 
+import json
+
+# Append the docpormpting home directory so we can do the imports
+sys.path.append(os.path.join(os.getcwd(), "../docprompting"))
+from dataset_helper.tldr.gen_metric import tldr_metrics
+
+# If needed, pull the results from json file and save two files (y and y_pred)
+# Where each line is a matching set of 
+y_pred = "temp_files/test_pred.txt"
+y_truth = "temp_files/test_target.txt"
+
+save_file_name = "temp_files/pred_metrics.json"
+
+metrics = tldr_metrics(y_truth, y_pred)
+
+# save metrics json file
+with open(save_file_name, "w") as f:
+    json.dump(metrics, f, indent=2)
+print(metrics)
